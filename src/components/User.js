1b6f0b9c0f6371b759498e3e4eb1ef9814ccc073
Fatih <fdfatihdurna@gmail.com>
2019-12-10 00:00:29 +0300

    ...
    ...

--- src/components/User.js
+++ src/components/User.js
@@ -1,36 +1,61 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class User extends Component {
-    render() {
-        const {name,department,salary} = this.props;
-        return (
-            <div className="col-md-8 mb-4">
-                <div className="card">
-                    <div className="card-header d-flex justify-content-between">
-                        <h4 className="d-inline">{name}</h4>
-                        <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
-                    </div>
-                    <div className="card-body">
-                        <p className="card-text">Maaş : {salary}</p>
-                        <p className="card-text">Department : {department}</p>
-                    </div>
-                </div>
+  onClickEvet() {
+    console.log(this);
+  }
+  myfunction() {
+    console.log("selam");
+  }
+  cardHeader = e => {
+    this.setState({
+      isVisible: !this.state.isVisible
+    });
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: true
+    };
+    this.onClickEvet = this.onClickEvet.bind(this);
+  }
+  render() {
+    const { name, department, salary } = this.props;
+    const { isVisible } = this.state;
+    return (
+      <div className="col-md-8 mb-4">
+        <div className="card">
+          <div
+            className="card-header d-flex justify-content-between"
+            onClick={this.cardHeader}
+          >
+            <h4 className="d-inline" onClick={this.onClickEvet}>
+              {name}
+            </h4>
+            <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
+          </div>
+          {isVisible ? (
+            <div className="card-body">
+              <p className="card-text">Maaş : {salary}</p>
+              <p className="card-text">Department : {department}</p>
+              <p>{this.state.test}</p>
             </div>
-        )
-    }
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
-
 
 User.defaultProps = {
-    name: "isimsiz",
-    salary : "parasız",
-    department : "departmansız"
-}
+  name: "isimsiz",
+  salary: "parasız",
+  department: "departmansız"
+};
 
 User.propTypes = {
-    name : PropTypes.string.isRequired,
-    salary : PropTypes.string.isRequired,
-    department : PropTypes.string.isRequired
-}
+  name: PropTypes.string.isRequired,
+  salary: PropTypes.string.isRequired,
+  department: PropTypes.string.isRequired
+};
