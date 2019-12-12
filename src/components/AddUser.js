22d401d48a87d03232dc6facf9d6964b2bc64d63
fatih.durna <fatih.durna@IST-FDURNA>
2019-12-12 15:57:51 +0300

    ...
    ...

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -19,15 +19,35 @@ const Animation = posed.div({
 
 class AddUser extends Component {
     state = {
-        visible : false
+        visible : false,
+        name : "",
+        department : "",
+        salary : ""
     }
     changeVisibility = (e) => {
         this.setState({
             visible : !this.state.visible
         })
     }
+    changeInput = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value,
+            [e.target.department] : e.target.value,
+            [e.target.salary] : e.target.value
+        })
+    }
+    addUser = (e) => {
+      e.preventDefault();
+      const {name,department,salary} = this.state;
+      const newUser = {
+          name:name,
+          department:department,
+          salary:salary
+      }
+      console.log(newUser);
+    }
     render() {
-        const {visible} = this.state;
+        const {visible,name,department,salary} = this.state;
         return (
             <div className="col-md-8 mb-4">
                 <button onClick={this.changeVisibility} className="btn btn-dark btn-block mb-2">{visible ? "Hide Form" : "Show Form"}</button>
@@ -37,19 +57,19 @@ class AddUser extends Component {
                         <h4>Add User Form</h4>
                     </div>
                     <div className="card-body">
-                        <form className="text-left">
+                        <form className="text-left" onSubmit={this.addUser}>
                             <div className="selam"></div>
                             <div className="form-group">
                                 <label htmlFor="name">Name</label>
-                                <input type="text" name="name" id="name" placeholder="Name" className="form-control" />
+                                <input type="text" onChange={this.changeInput} value={name} name="name" id="name" placeholder="Name" className="form-control" />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="department">Department</label>
-                                <input type="text" name="department" id="department" placeholder="Department" className="form-control" />
+                                <input type="text" onChange={this.changeInput} value={department} name="department" id="department" placeholder="Department" className="form-control" />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="salary">Salary</label>
-                                <input type="text" name="salary" id="salary" placeholder="Salary" className="form-control" />
+                                <input type="text" onChange={this.changeInput} value={salary} name="salary" id="salary" placeholder="Salary" className="form-control" />
                             </div>
                             <button type="submit" className="btn btn-danger btn-block">Add User</button>
                         </form>
