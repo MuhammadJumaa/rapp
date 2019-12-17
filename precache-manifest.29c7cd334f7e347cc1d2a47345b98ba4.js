aa1d875f03819e416bf387d7a96afd0c79555b89
fatih.durna <fatih.durna@IST-FDURNA>
2019-12-17 17:43:32 +0300

    Updates

---
+++ precache-manifest.29c7cd334f7e347cc1d2a47345b98ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9f35439c4592da5e021d5a61474154",
+    "url": "/rapp/index.html"
+  },
+  {
+    "revision": "eddd945269201eba76b6",
+    "url": "/rapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "16f5b2dd056ac980d745",
+    "url": "/rapp/static/js/2.c7321052.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/rapp/static/js/2.c7321052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eddd945269201eba76b6",
+    "url": "/rapp/static/js/main.bfaf3e16.chunk.js"
+  },
+  {
+    "revision": "747ca69c43974540fa6f",
+    "url": "/rapp/static/js/runtime-main.72f2b8a0.js"
+  }
+]);
